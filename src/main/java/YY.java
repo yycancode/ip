07005cd307b0839afc5542ee93b88276fb691f1c
yycancode <111871106+yycancode@users.noreply.github.com>
2025-08-20 22:30:23 +0800

    Level-2

--- src/main/java/YY.java
+++ src/main/java/YY.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class YY {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         String line = "________________________________________";
 
         System.out.println(line);
@@ -12,15 +14,24 @@ public class YY {
 
         while (true) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(line);
+            } else {
+                tasks.add(input);
+                System.out.println(line);
+                System.out.println("added: " + input);
+                System.out.println(line);
             }
-            System.out.println(line);
-            System.out.println(" " + input);
-            System.out.println(line);
         }
     }
 }
