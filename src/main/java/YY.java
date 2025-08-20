ea666902501652bae9fcbf40c2d39a562fae1afe
yycancode <111871106+yycancode@users.noreply.github.com>
2025-08-20 21:52:54 +0800

    Level-1

--- src/main/java/YY.java
+++ src/main/java/YY.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class YY {
     public static void main(String[] args) {
-        String message = "Hello! I'm YY!\nWhat can I do for you?\n\nBye. Hope to see you again soon!";
-        System.out.printf(message);
+        Scanner sc = new Scanner(System.in);
+        String line = "________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm YY!");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
     }
 }
