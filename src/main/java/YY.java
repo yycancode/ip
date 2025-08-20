c4c9e26326a1401f8ea490c3d6e835686e2302ef
yycancode <111871106+yycancode@users.noreply.github.com>
2025-08-20 21:01:13 +0800

    Level-0

---
+++ src/main/java/YY.java
@@ -0,0 +1,6 @@
+public class YY {
+    public static void main(String[] args) {
+        String message = "Hello! I'm YY!\nWhat can I do for you?\n\nBye. Hope to see you again soon!";
+        System.out.printf(message);
+    }
+}
