378c239b06f1282b5e2d00f808b37de6ce27ec84
yycancode <111871106+yycancode@users.noreply.github.com>
2025-09-16 10:43:28 +0800

    Add name for chatbot in window title

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("YY");
             stage.setMinHeight(220);
             stage.setMinWidth(417);
             stage.setMaxWidth(417);
